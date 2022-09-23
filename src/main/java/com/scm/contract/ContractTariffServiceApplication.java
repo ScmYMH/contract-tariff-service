d0191547029ef048acece6f01c138b21a5df11b4
ayeon <gudwodjssl@gmail.com>
2022-09-23 10:35:52 +0900

    port 수정

--- src/main/java/com/scm/contract/ContractTariffServiceApplication.java
+++ src/main/java/com/scm/contract/ContractTariffServiceApplication.java
@@ -2,10 +2,10 @@ package com.scm.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ContractTariffServiceApplication {
 
     public static void main(String[] args) {
